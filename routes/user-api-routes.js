4a4f5462df5cccbe340157929f497f6d048b8c40
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-10 14:52:18 -0800

    modeling and routing continue

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -10,16 +10,16 @@ var db = require("../models");
 
 module.exports = function(app) {
   // If we want to add a feature to show all users 
-  app.get("/api/users", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Task
-    db.User.findAll({
-      include: [db.Task]
-    }).then(function(dbUser) {
-      res.json(dbUser);
-    });
-  });
+  // app.get("/api/users", function(req, res) {
+  //   // Here we add an "include" property to our options in our findAll query
+  //   // We set the value to an array of the models we want to include in a left outer join
+  //   // In this case, just db.Task
+  //   db.User.findAll({
+  //     include: [db.Task]
+  //   }).then(function(dbUser) {
+  //     res.json(dbUser);
+  //   });
+  // });
   // GET request for a specific user, if we want to add a way to look at other user's profiles/star rating/etc
   app.get("/api/users/:id", function(req, res) {
     // Here we add an "include" property to our options in our findOne query
