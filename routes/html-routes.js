cc934d6c8e6afa93a08b59306bacd2e13ceafa94
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-09 16:06:42 -0800

    restructure schema and make task model; add HTML route for GET request to root route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    // res.sendFile(path.join(__dirname, "../public/.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/docs/landingPage.html"));
   });
 
   // cms route loads cms.html
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the sparrow_db if it exists currently --
+DROP DATABASE IF EXISTS sparrow_db;
+-- Creates the "sparrow_db" database --
+CREATE DATABASE sparrow_db;
