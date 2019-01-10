740a41c6f0527d8d5c1c02e6b6f191cea67e3fbb
saranyamohandas <saranyamohandas2@gmail.com>
2019-01-10 14:25:42 -0800

    update sigin/signup feature

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,7 +1,51 @@
 //var authController = require('../controllers/authcontroller.js');
+//var express = require("express");
+//var passport = require("passport");
+var passport = require("../config/passport/passport.js")
+//console.log("passport",passport);
+module.exports = function(app,passport) {
  
-module.exports = function(app) {
+  app.get('/signup', function(req, res) {
+     res.render("signup");
+ });
+    app.get('/signin', function(req, res) {
+     res.render("signin");
+ });
+    app.get("/dashboard",isLoggedIn,function(req,res){
+        res.render("dashboard");
+    })
+    
+   app.get("/logout",function(req,res){
+       console.log("you hit me")
+       req.session.destroy(function(err) {
+           if(err) console.log(err)
+           //req.logout();
+           res.redirect('/signup');
+       });
+   });
+    
+   app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/dashboard',
  
-   // app.get('/signup', signup);
+        failureRedirect: '/signup'
+    }
+                                            ));
+    app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/dashboard',
+ 
+        failureRedirect: '/signin'
+    }
+                                             ));
+    
+   function isLoggedIn(req, res, next) {
+            if (req.isAuthenticated())
+                return next();
+    res.redirect('/signin');
+ 
+}                                       
+                                             
+ 
+
+    
  
 }
