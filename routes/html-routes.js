93d17f34816f13c9e5a11b6345e7b1accbf6acbf
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-10 21:39:10 -0800

    update models and routing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,29 +9,43 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  app.get("/", function(req, res) {
+  // Root route loads landingPage.html
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/assets/docs/landingPage.html"));
   });
-
-  // login page
-//  app.get("/signup", function(req, res) {
-//     res.render("signup");
-// });
-//
-//  // blog route loads blog.html
-//  app.get("/login", function(req, res) {
-//      res.render("login");
-//    // res.sendFile(path.join(__dirname, "../public/.html"));
-//  });
-
-  // authors route loads author-manager.html
-  app.get("/", function(req, res) {
-    // res.sendFile(path.join(__dirname, "../public/.html"));
+  // Route to User's "Personal" tasks list
+  app.get("/lists/personal", function (req, res) {
+    // Change to render to Handlebars template when they're complete
+    res.sendFile(path.join(__dirname, "../public/assets/docs/documentation.html"));
   });
+  // Route to User's "Outsourced" tasks list
+  app.get("/lists/outsourced", function (req, res) {
+    // Change to render to Handlebars template when they're complete
+    res.sendFile(path.join(__dirname, "../public/assets/docs/outsourced.html"));
+  });
+  // Route to User's "Claimed" tasks list
+  app.get("/lists/claimed", function (req, res) {
+    // Change to render to Handlebars template when they're complete
+    res.sendFile(path.join(__dirname, "../public/assets/docs/claimed.html"));
+  });
+  // Route to "Global" tasks list
+  app.get("/lists/global", function (req, res) {
+    // Change to render to Handlebars template when they're complete
+    res.sendFile(path.join(__dirname, "../public/assets/docs/global.html"));
+  });
+  // Route to update User's profile page
+  app.get("/update/profile", function (req, res) {
+    // Could potentially use a DB query and Handlebars to render User's current information into the placeholders of the input fields
+    res.sendFile(path.join(__dirname, "../public/assets/docs/sparrowUser.html"));
+  });
+  // Route to create a new Task
+  app.get("/tasks/new", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/assets/docs/tasksTemplate.html"));
+  });
+  // Routes to Sign In, Sign Up, and Log Out are handled in the auth-routes.js
 
 };
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -8,7 +8,7 @@
 // Requiring our models
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // If we want to add a feature to show all users 
   // app.get("/api/users", function(req, res) {
   //   // Here we add an "include" property to our options in our findAll query
@@ -21,46 +21,50 @@ module.exports = function(app) {
   //   });
   // });
   // GET request for a specific user, if we want to add a way to look at other user's profiles/star rating/etc
-  app.get("/api/users/:id", function(req, res) {
+  app.get("/api/users/:id", function (req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Task
-    db.Author.findOne({
+    db.User.findOne({
       where: {
         id: req.params.id
       },
       include: [db.Task]
-    }).then(function(dbUser) {
+    }).then(function (dbUser) {
       res.json(dbUser);
     });
   });
   // POST request for creating a new user with form data from the DOM
-  app.post("/api/users", function(req, res) {
-    db.User.create(req.body).then(function(dbUser) {
+  app.post("/api/users", function (req, res) {
+    db.User.create(req.body).then(function (dbUser) {
       res.json(dbUser);
     });
   });
   // PUT route for updating user information based on form data sent from DOM and characteristic (unique user id) of 'update' button clicked on the 'body' of the page
-  app.put("/api/users", function(req, res) {
+  app.put("/api/users", function (req, res) {
     db.User.update(
       req.body,
       {
         where: {
           id: req.body.id
         }
-      }).then(function(dbUser) {
-      res.json(dbUser);
-    });
+      }).then(function (dbUser) {
+        res.json(dbUser);
+      });
   });
   // If we want to add a 'delete account' feature
-  app.delete("/api/users/:id", function(req, res) {
+  app.delete("/api/users/:id", function (req, res) {
     db.User.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbUser) {
-      res.json(dbUser);
+    }).then(function (dbUser) {
+      // Redirect unregistered user to home page
+      res.redirect("/");
     });
   });
-
+  // Route to redirect all invalid URL inputs to the homepage - included here because it's the last routing file included in server.js
+  app.all('*', function (req, res) {
+    res.redirect("/");
+  });
 };
