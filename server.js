eea8ac63caa700961ceb1ad5368e7cb85d5efa03
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-01-15 09:13:03 -0800

    deploy to heroku

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.set("view engine", "handlebars");
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
